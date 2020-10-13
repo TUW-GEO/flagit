1e6739381787b732891a2c03dd43cd5bbc358d93
Daniel Aberer <daniel.aberer@tuwien.ac.at>
2020-10-13 19:11:13 +0200

    Code and documentation upgrade

---
+++ src/flagit/settings.py
@@ -0,0 +1,11 @@
+class Variables():
+    """
+    Class for flagging thresholds of soil moisture, accompanying ancillary observations and
+    """
+    sm_lower = 0
+    sm_upper = 60
+    ta_lower = 0
+    ts_lower = 0
+    p_min = 0.2
+
+    plateau_count = 0
