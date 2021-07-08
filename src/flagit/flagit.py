e52292963711f0375f52f3b649baf9344f386c99
daberer <daniel.aberer@geo.tuwien.ac.at>
2021-07-08 11:17:24 +0200

    Adjust for ISMN use

--- src/flagit/flagit.py
+++ src/flagit/flagit.py
@@ -45,6 +45,12 @@ class Interface(object):
     For a detailed description of the algorithms please see: Dorigo, W. A., Xaver, A., Vreugdenhil, M., Gruber, A.,
     Hegyiova, A., Sanchis-Dufau, A. D., ... & Drusch, M. (2013). Global automated quality control of in situ soil
     moisture data from the International Soil Moisture Network. Vadose Zone Journal, 12(3), doi:10.2136/vzj2012.0097.
+    
+    If variable is not soil moisture but one of the following list: 
+    [soil temperature, air temperature, precipitation, soil suction, snow water equivalent, snow depth, 
+    soil surface temperature] then for the ISMN quality control only threshold flags (c01 and c02) are applied.
+    
+    It is required that the first column of the dataframe is equal to the name of the variable.   
 
     Parameters
     ----------
@@ -74,25 +80,28 @@ class Interface(object):
 
     """
 
-    def __init__(self, data, sat_point=None):
+    def __init__(self, data, sat_point=None, depth_from = None):
         self.data = data
         self.sat_point = sat_point
+        self.depth_from = depth_from
+        self.variable = None
 
         if not type(self.data) == pd.DataFrame:
             raise FormatError('Please provide pandas.DataFrame as data.')
 
         if 'soil_moisture' not in self.data.columns:
-            raise FormatError('\n\nInput DataFrame requires columns "soil_moisture"\noptional columns: '
-                              '"soil_temperature", "air_temperature", "precipitation", "gldas_soil_temperature", '
-                              '"gldas_precipitation"')
-
-        self.data['qflag'] = data.soil_moisture.apply(lambda x: set())
+            self.variable = self.data.keys()[0]
+            self.data['qflag'] = data[self.variable].apply(lambda x: set())
+        
+        else:
+            self.data['qflag'] = data.soil_moisture.apply(lambda x: set())
+            self.variable = 'soil_moisture'
 
         if not pd.infer_freq(self.data.index) == 'H':
             warnings.warn('ISMN automated quality control were developed for hourly data.')
 
 
-    def run(self, name=None, sat_point=None) -> pd.DataFrame:
+    def run(self, name=None, sat_point=None, depth_from=None, flag_numbers=False) -> pd.DataFrame:
         """
         Applies all quality control algorithms when keyword name is not set. However for flag C03 a threshold value
         (saturation point: highest soil moisture value depending on soil properties) is needed as input.
@@ -107,6 +116,9 @@ class Interface(object):
                 and organic content for each station using Equations [2,3,5] from Saxton & Rawls (2006).
                 (Saxton, K. E., & Rawls, W. J. (2006). Soil water characteristic estimates by texture and organic matter
                 for hydrologic solutions. Soil science society of America Journal, 70(5), 1569-1578.)
+        flag_numbers : bool
+                if true flag numbers are used as tags in the qflag column instead of flag ids (e.g.: '1' instead of
+                'C01', '14' instead of 'G')
 
         Returns
         -------
@@ -116,22 +128,31 @@ class Interface(object):
         keys = self.data.keys()
         if not self.sat_point:
             self.sat_point = sat_point
-        self.apply_savgol()
-        flags_dict = {'C01': self.flag_C01, 'C02': self.flag_C02, 'C03': self.flag_C03, 'D01': self.flag_D01,
+        if not self.depth_from:
+            self.depth_from = depth_from
+        if self.variable == 'soil_moisture':
+            self.apply_savgol()
+        if not flag_numbers:
+            flags_dict = {'C01': self.flag_C01, 'C02': self.flag_C02, 'C03': self.flag_C03, 'D01': self.flag_D01,
                       'D02': self.flag_D02, 'D03': self.flag_D03, 'D04': self.flag_D04, 'D05': self.flag_D05,
                       'D06': self.flag_D06, 'D07': self.flag_D07, 'D09': self.flag_D09, 'D10': self.flag_D10,
                       'G': self.flag_G}
+        else:
+            flags_dict = {1: self.flag_C01, 2: self.flag_C02, 3: self.flag_C03, 4: self.flag_D01,
+                          5: self.flag_D02, 6: self.flag_D03, 7: self.flag_D04, 8: self.flag_D05,
+                          9: self.flag_D06, 10: self.flag_D07, 12: self.flag_D09, 13: self.flag_D10,
+                          14: self.flag_G}
 
 
         if name is not None:
             if type(name) == list:
                 for key in name:
-                    flags_dict[key]()
+                    flags_dict[key](key)
             elif type(name) == str:
-                flags_dict[name]()
+                flags_dict[name](name)
         else:
             for key in flags_dict.keys():
-                flags_dict[key]()
+                flags_dict[key](key)
 
         return self.data[keys]
 
@@ -166,71 +187,103 @@ class Interface(object):
         self.data['deriv1'] = savgol(self.data.soil_moisture, 3, 2, 1, mode='nearest')
         self.data['deriv2'] = savgol(self.data.soil_moisture, 3, 2, 2, mode='nearest')
 
-    def flag_C01(self):
+    def flag_C01(self, tag):
         """
         Soil moisture below threshold:
         Flags when measurement is below threshold
-        """
-        low_boundary = t.sm_lower
-        index = self.data[self.data.soil_moisture < low_boundary].index
-        if len(index):
-            self.data['qflag'][index].apply(lambda x: x.add('C01'))
 
-    def flag_C02(self):
+        Parameters
+        ----------
+        tag : string or int, optional
+        code added to qflag-column when flag-criteria are met
+        """
+        low_boundary = t.low_boundary(self.variable)
+
+        index = self.data[self.data[self.variable] < low_boundary].index
+            
+        if len(index):
+            self.data['qflag'][index].apply(lambda x: x.add(tag))
+
+    def flag_C02(self, tag):
         """
         Soil moisture above threshold:
         Flags when measurement is above threshold
-        """
-        upper_boundary = t.sm_upper
-        index = self.data[self.data.soil_moisture > upper_boundary].index
-        if len(index):
-            self.data['qflag'][index].apply(lambda x: x.add('C02'))
 
-    def flag_C03(self):
+        Parameters
+        ----------
+        tag : string or int, optional
+        code added to qflag-column when flag-criteria are met
+        """
+        upper_boundary = t.hi_boundary(self.variable)
+        index = self.data[self.data[self.variable] > upper_boundary].index
+        if len(index):
+            self.data['qflag'][index].apply(lambda x: x.add(tag))
+
+    def flag_C03(self, tag):
         """
         Soil moisture above saturation point:
         Flags when soil moisture is above saturation point.
         At ISMN the saturation point is calculated from Harmonized World Soil Database (HWSD) sand, clay and organic
         content for each station using Equations [2,3,5] from Saxton & Rawls (2006). doi:10.2136/vzj2012.0097.
+        
+        Parameters
+        ----------
+        tag : string
+        code added to qflag-column when flag-criteria are met
         """
         if not self.sat_point:
             return
 
         index = self.data.loc[self.data.soil_moisture > self.sat_point].index
         if len(index):
-            self.data['qflag'][index].apply(lambda x: x.add('C03'))
+            self.data['qflag'][index].apply(lambda x: x.add(tag))
 
-    def flag_D01(self):
+    def flag_D01(self, tag):
         """
         In situ soil temperature below threshold:
         Flags when ancillary in situ soil temperature is below threshold
+        
+        Parameters
+        ----------
+        tag : string or int, optional
+        code added to qflag-column when flag-criteria are met
         """
         if 'soil_temperature' in self.data.columns:
-            index = self.data[self.data.soil_temperature < t.ts_lower].index
+            index = self.data[self.data.soil_temperature < t.ancillary_ts_lower].index
             if len(index):
-                self.data['qflag'][index].apply(lambda x: x.add('D01'))
+                self.data['qflag'][index].apply(lambda x: x.add(tag))
 
-    def flag_D02(self):
+    def flag_D02(self, tag):
         """
         In situ air temperature below threshold:
         Flags when ancillary in situ air temperature is below threshold
+        
+        Parameters
+        ----------
+        tag : string or int, optional
+        code added to qflag-column when flag-criteria are met
         """
         if 'air_temperature' in self.data.columns:
-            index = self.data[self.data['air_temperature'] < t.ta_lower].index
+            index = self.data[self.data['air_temperature'] < t.ancillary_ta_lower].index
             if len(index):
-                self.data['qflag'][index].apply(lambda x: x.add('D02'))
+                self.data['qflag'][index].apply(lambda x: x.add(tag))
 
-    def flag_D03(self):
+    def flag_D03(self, tag):
         """
         GLDAS soil temperature below threshold:
         Flags when ancillary GLDAS NOAA soil temperature is below threshold
+        
+        Parameters
+        ----------
+        tag : string or int, optional
+        code added to qflag-column when flag-criteria are met
         """
         if 'gldas_soil_temperature' in self.data.columns:
-            index = self.data[self.data['gldas_soil_temperature'] < t.ts_lower].index
+            index = self.data[self.data['gldas_soil_temperature'] < t.ancillary_ts_lower].index
             if len(index):
-                self.data['qflag'][index].apply(lambda x: x.add('D03'))
+                self.data['qflag'][index].apply(lambda x: x.add(tag))
 
-    def flag_D04(self):
+    def flag_D04(self, tag):
         """
         Soil moisture rise without precipitation event (in situ):
         Flags when soil moisture increased both during the last hour and during the preceding 24h (increase is larger
@@ -238,11 +291,21 @@ class Interface(object):
         or equal to the minimum precipitation (depending on sensor depth).
     
         At ISMN this flag is only applied to surface soil moisture sensors (<= 10cm sensor depth)
+        
+        Parameters
+        ----------
+        tag : string or int, optional
+        code added to qflag-column when flag-criteria are met
         """
 
         if 'precipitation' in self.data.columns:
-            min_precipitation = t.p_min
-            self.data['total_precipitation'] = self.data['precipitation'].rolling(min_periods=1, window=24).sum()
+
+            if self.depth_from != None:
+                min_precipitation = float(self.depth_from) * 0.05 * 0.5 * 1000
+            if not min_precipitation:
+                min_precipitation = t.ancillary_p_min
+            self.data['total_precipitation'] = round(self.data['precipitation']
+                                                     .rolling(min_periods=1, window=24).sum(), 1)
 
             self.data['std_x2'] = self.data['soil_moisture'].rolling(min_periods=1, window=25).std() * 2
             self.data['rise24h'] = self.data['soil_moisture'].diff(24)
@@ -251,9 +314,9 @@ class Interface(object):
             index = self.data[(self.data['rise1h'] > 0) & (self.data['rise24h'] > self.data['std_x2']) &
                               (self.data['total_precipitation'] < min_precipitation)].index
 
-            self.data['qflag'][index].apply(lambda x: x.add('D04'))
+            self.data['qflag'][index].apply(lambda x: x.add(tag))
 
-    def flag_D05(self):
+    def flag_D05(self, tag):
         """
         Soil moisture rise without precipitation event (Gldas precipitation):
         Flags when soil moisture increased both during the last hour and during the preceding 24h (increase is larger
@@ -261,10 +324,15 @@ class Interface(object):
         or equal to the minimum precipitation (depending on sensor depth).
     
         At ISMN this flag is only applied to surface soil moisture sensors (<= 10cm sensor depth)
+        
+        Parameters
+        ----------
+        tag : string or int, optional
+        code added to qflag-column when flag-criteria are met
         """
 
         if 'gldas_precipitation' in self.data.columns:
-            min_precipitation = t.p_min
+            min_precipitation = t.ancillary_p_min
 
             self.data['gldas_total_precipitation'] = self.data['gldas_precipitation'].rolling(min_periods=1,
                                                                                               window=24).sum()
@@ -276,14 +344,19 @@ class Interface(object):
                               (self.data['gldas_total_precipitation'] < min_precipitation)].index
 
             if len(index):
-                self.data['qflag'][index].apply(lambda x: x.add('D05'))
+                self.data['qflag'][index].apply(lambda x: x.add(tag))
 
-    def flag_D06(self):
+    def flag_D06(self, tag):
         """
         Soil moisture spike:
         Flags when soil moisture shows a positive or negative spike.
         See Eq [4,5,6] in Dorigo et al. (2013), Global Automated Quality Control of In Situ
         Soil Moisture Data from the International Soil Moisture Network,VZJ.
+        
+        Parameters
+        ----------
+        tag : string or int, optional
+        code added to qflag-column when flag-criteria are met
         """
 
         def rolling_var(sm_array) -> float:
@@ -362,9 +435,9 @@ class Interface(object):
 
         index = self.data[(self.data.spike > 0) | ((self.data.spike.shift(1) > 0) & (self.data['spike_2h'] > 0))].index
 
-        self.data['qflag'][index].apply(lambda x: x.add('D06'))
+        self.data['qflag'][index].apply(lambda x: x.add(tag))
 
-    def flag_D07(self):
+    def flag_D07(self, tag):
         """
         !Includes jumps (D08)!
         Soil moisture drop (D07) or jump (D08):
@@ -378,12 +451,17 @@ class Interface(object):
         Soil Moisture Data from the International Soil Moisture Network,VZJ.
 
         Includes an alternative drop type, which was not included in VJZ paper: drop from above 0.05m³/m³ to zero
+        
+        Parameters
+        ----------
+        tag : string or int, optional
+        code added to qflag-column when flag-criteria are met
         """
 
         self.data['absolute_change'] = self.data['soil_moisture'] - self.data['soil_moisture'].shift(1)
         self.data['eq7'] = abs(self.data['absolute_change'].div(self.data['soil_moisture']))
         self.data['eq8'] = abs(self.data['deriv1'].rolling(min_periods=4, window=25, center=True).mean() * 10)
-        self.data['eq9'] = abs(self.data['deriv2'].shift(1).div(self.data['deriv2']))
+        self.data['eq9'] = round(abs(self.data['deriv2'].shift(1).div(self.data['deriv2'])), 1)
 
         self.data['eq9a'] = abs(self.data['deriv2'].div(self.data['deriv2'].shift(-2)))
 
@@ -406,11 +484,17 @@ class Interface(object):
         index_neg = index_neg.append(index_zero)
 
         if len(index_neg):
-            self.data['qflag'][index_neg].apply(lambda x: x.add('D07'))
+            self.data['qflag'][index_neg].apply(lambda x: x.add(tag))
+
+        # Change tag to indicate soil moisture jumps
+        if isinstance(tag, int):
+            tag += 1
+        elif tag == 'D07':
+            tag = 'D08'
 
         # Includes soil moisture jumps (flag D08)
         if len(index_pos):
-            self.data['qflag'][index_pos].apply(lambda x: x.add('D08'))
+            self.data['qflag'][index_pos].apply(lambda x: x.add(tag))
 
 
     def flag_D08(self):
@@ -420,7 +504,7 @@ class Interface(object):
         pass
 
 
-    def flag_D09(self):
+    def flag_D09(self, tag):
         """
         Low constant values:
         Flags where a previous soil moisture break (D07) and a period of low relative variance
@@ -429,6 +513,11 @@ class Interface(object):
 
         See Eq [14] in Dorigo et al. (2013), Global Automated Quality Control of In Situ
         Soil Moisture Data from the International Soil Moisture Network,VZJ.
+        
+        Parameters
+        ----------
+        tag : string or int, optional
+        code added to qflag-column when flag-criteria are met
         """
 
         self.data.dropna(subset=['soil_moisture'], inplace=True)
@@ -475,14 +564,14 @@ class Interface(object):
         index = self.data[self.data['end'] > 0.0].index
 
         if len(index):
-            self.data['qflag'][index].apply(lambda x: x.add('D09'))
+            self.data['qflag'][index].apply(lambda x: x.add(tag))
 
         if type(self.data.index) == pd.core.indexes.datetimes.DatetimeIndex:
             self.data = self.data.resample('H').asfreq()
 
 
 
-    def flag_D10(self):
+    def flag_D10(self, tag):
         """
         Invariant high soil moisture values:
         Flags where the variance of soil moisture values within 12h is below 0.05 ->  period of low variance (plv)
@@ -491,9 +580,13 @@ class Interface(object):
         soil moisture values between the rise and drop (or if they occur beyond plv scope, beginning and/or respective
         end of plv) of above 0.95% of the previous highest soil moisture value ever detected (highest_sm).
 
-
         See Eq [10,11,12,13] in Dorigo et al. (2013), Global Automated Quality Control of In Situ
         Soil Moisture Data from the International Soil Moisture Network,VZJ.
+        
+        Parameters
+        ----------
+        tag : string or int, optional
+        code added to qflag-column when flag-criteria are met
         """
 
         def renumber_plateaus(array) -> list:
@@ -521,7 +614,9 @@ class Interface(object):
 
         # Mean of plateau must be higher than 95% of this threshold;
         # For ISMN quality flags the previous 2 years of data are taken into account.
-        highest_sm = self.data['soil_moisture'][self.data['soil_moisture'] < 60].max()
+        highest_sm_value = self.data['soil_moisture'][self.data['soil_moisture'] < 60].max()        
+        
+        
 
         # Throw out datagaps - plateau can bridge gap
         self.data.dropna(subset=['soil_moisture'], inplace=True)
@@ -569,18 +664,28 @@ class Interface(object):
                     min_search_period_end = VAR_period[::-1].idxmax()
 
             Plateau = self.data['soil_moisture'].loc[max_search_period_start:min_search_period_end]
-            if Plateau.mean() > (highest_sm * 0.95):
-                index.extend(Plateau.index)
+            if 'highest_sm' in self.data.columns:
+                if Plateau.mean() > \
+                        (self.data['highest_sm'].loc[max_search_period_start:min_search_period_end].mean() * 0.95):
+                    index.extend(Plateau.index)
+            else: # if no highest_sm column then use highest_sm_value as threshold
+                if Plateau.mean() > (highest_sm_value * 0.95):
+                    index.extend(Plateau.index)
 
-        self.data['qflag'][index].apply(lambda x: x.add('D10'))
+        self.data['qflag'][index].apply(lambda x: x.add(tag))
 
-        if type(self.data.index) == pd.core.indexes.datetimes.DatetimeIndex:
-            self.data = self.data.resample('H').asfreq()
+        # if type(self.data.index) == pd.core.indexes.datetimes.DatetimeIndex:
+        #     self.data = self.data.resample('H').asfreq()
 
 
-    def flag_G(self):
+    def flag_G(self, tag):
         """
-       Applies flag 'G' for 'good' to all unflagged observations
+        Applies tag for all unflagged observations
+        
+        Parameters
+        ----------
+        tag : string or int, optional
+        code added to qflag-column when flag-criteria are met
         """
         index_good = self.data[self.data['qflag'] == set()].index
-        self.data['qflag'][index_good].apply(lambda x: x.add('G'))
+        self.data['qflag'][index_good].apply(lambda x: x.add(tag))
